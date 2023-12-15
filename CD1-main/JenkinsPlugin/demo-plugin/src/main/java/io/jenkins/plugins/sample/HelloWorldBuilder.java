41521925e0805d41f895132b042d74daaa0dbc6d
BarrettBytes <131525111+BarrettBytes@users.noreply.github.com>
2023-12-15 22:31:53 +1030

    begin work

--- CD1-main/JenkinsPlugin/demo-plugin/src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
+++ CD1-main/JenkinsPlugin/demo-plugin/src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
@@ -1,54 +1,86 @@
 package io.jenkins.plugins.sample;
 
-import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
-import hudson.plugins.git.UserRemoteConfig;
-import hudson.plugins.git.SubmoduleConfig;
-import hudson.plugins.git.extensions.GitSCMExtension;
 import com.cloudbees.plugins.credentials.CredentialsMatchers;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
-import hudson.security.ACL;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
 
-import hudson.plugins.git.GitSCM;
-import hudson.plugins.git.extensions.impl.CloneOption;
-import org.jenkinsci.plugins.gitclient.GitClient;
-import hudson.plugins.git.BranchSpec;
-import java.util.Collections;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractProject;
-import hudson.model.queue.QueueTaskFuture;
+import hudson.model.Item;
+import hudson.model.Job;
+import hudson.security.Permission;
+import hudson.model.Queue.Task;
+import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import hudson.tasks.BuildStepDescriptor;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import hudson.model.Result;
-import java.io.InputStream;
-
+import hudson.plugins.git.BranchSpec;
+import hudson.plugins.git.GitSCM;
+import hudson.plugins.git.UserRemoteConfig;
+import hudson.plugins.git.extensions.GitSCMExtension;
+import hudson.plugins.git.extensions.impl.CloneOption;
+import hudson.security.ACL;
+import hudson.security.AccessControlled;
 import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
 import hudson.util.FormValidation;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import hudson.model.Job;
-
-import java.nio.file.Files;
-import java.util.Queue;
-
-import javax.servlet.ServletException;
+import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
 import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.StepDescriptor;
+import org.jenkinsci.plugins.workflow.steps.StepExecution;
+import org.jenkinsci.plugins.workflow.steps.StepExecutionIterator;
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper;
+import org.jenkinsci.plugins.workflow.steps.SynchronousStepExecution;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
-import groovy.lang.GroovyShell;
-import groovy.lang.Binding;
+import org.kohsuke.stapler.verb.POST;
 
-import jenkins.model.Jenkins;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
+
+import javax.servlet.ServletException;
+
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper;
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import hudson.model.Item;
+import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
+
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import hudson.security.ACL;
+import jenkins.model.Jenkins;
+import hudson.util.ListBoxModel;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
+import hudson.plugins.git.SubmoduleConfig;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
 
 
 public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
@@ -60,6 +92,45 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
     private String SUDO_password;
     private String repositoryUrl;
     private boolean enableGitCheckout;
+
+    public ListBoxModel doFillCredentialsIdItems(@AncestorInPath Item item, @QueryParameter String credentialsId) {
+        ListBoxModel result = new ListBoxModel();
+        result.add(""); // Add an empty option
+    
+        // Initialize variables
+        boolean foundCurrentValue = false;
+        String id = ""; // You need to define the appropriate type for 'id'
+    
+        // Populate the list with credentials
+        for (StandardUsernamePasswordCredentials cred : CredentialsProvider.lookupCredentials(
+                StandardUsernamePasswordCredentials.class, Jenkins.getInstance(), ACL.SYSTEM, Collections.emptyList())) {
+            result.add(cred.getUsername(), cred.getId());
+    
+            // Check if the current credential is the selected one
+            if (!foundCurrentValue && cred.getId().equals(credentialsId)) {
+                foundCurrentValue = true;
+            }
+        }
+    
+        // If the current value wasn't found, add it as a new option
+        if (!foundCurrentValue && credentialsId != null && !credentialsId.isEmpty()) {
+            result.add(credentialsId, credentialsId);
+    
+            // Set the selected option
+            for (ListBoxModel.Option option : result) {
+                if (option.value.equals(credentialsId)) {
+                    option.selected = true;
+                    break;
+                }
+            }
+        }
+    
+        return result;
+    }
+    
+    
+
+
     @DataBoundConstructor
     public HelloWorldBuilder(String name) {
         this.name = name;
@@ -91,7 +162,6 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         this.useFrench = useFrench;
     }
 
-    
     private void checkoutRepository(FilePath workspace, TaskListener listener, Run<?, ?> run, Launcher launcher, String repositoryUrl, StandardUsernamePasswordCredentials gitCredentials)
     throws InterruptedException, IOException {
     try {
