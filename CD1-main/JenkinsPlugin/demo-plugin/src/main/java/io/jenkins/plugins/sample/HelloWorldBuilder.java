c8f98cdb6437b06a095dbb348c2e951ceb4c81de
BarrettBytes <131525111+BarrettBytes@users.noreply.github.com>
2024-02-07 23:38:39 +1030

    Attaching credentials (#1)
    progress: warning need to download 2.443 of jenkins to run now-replace current jenkins.war with this

--- CD1-main/JenkinsPlugin/demo-plugin/src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
+++ CD1-main/JenkinsPlugin/demo-plugin/src/main/java/io/jenkins/plugins/sample/HelloWorldBuilder.java
@@ -1,54 +1,105 @@
 package io.jenkins.plugins.sample;
 
-import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
-import hudson.plugins.git.UserRemoteConfig;
-import hudson.plugins.git.SubmoduleConfig;
-import hudson.plugins.git.extensions.GitSCMExtension;
 import com.cloudbees.plugins.credentials.CredentialsMatchers;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
-import hudson.security.ACL;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
+import org.kohsuke.stapler.interceptor.RequirePOST;
 
-import hudson.plugins.git.GitSCM;
-import hudson.plugins.git.extensions.impl.CloneOption;
-import org.jenkinsci.plugins.gitclient.GitClient;
-import hudson.plugins.git.BranchSpec;
-import java.util.Collections;
+
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractProject;
-import hudson.model.queue.QueueTaskFuture;
+import hudson.model.Item;
+import hudson.model.Job;
+import hudson.security.Permission;
+import hudson.model.Queue.Task;
+import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.TaskListener;
-import hudson.tasks.BuildStepDescriptor;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import hudson.model.Result;
-import java.io.InputStream;
+import hudson.plugins.git.BranchSpec;
+import hudson.plugins.git.GitSCM;
+import hudson.plugins.git.UserRemoteConfig;
+import hudson.plugins.git.extensions.GitSCMExtension;
+import hudson.plugins.git.extensions.impl.CloneOption;
+import hudson.security.ACL;
+import hudson.security.AccessControlled;
+import com.cloudbees.plugins.credentials.common.StandardCredentials;
+import hudson.util.ListBoxModel;
 
 import hudson.tasks.Builder;
+import hudson.tasks.BuildStepDescriptor;
 import hudson.util.FormValidation;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import hudson.model.Job;
-
-import java.nio.file.Files;
-import java.util.Queue;
-
-import javax.servlet.ServletException;
+import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
 import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.StepDescriptor;
+import org.jenkinsci.plugins.workflow.steps.StepExecution;
+import org.jenkinsci.plugins.workflow.steps.StepExecutionIterator;
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper;
+import org.jenkinsci.plugins.workflow.steps.SynchronousStepExecution;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 import org.kohsuke.stapler.QueryParameter;
-import groovy.lang.GroovyShell;
-import groovy.lang.Binding;
+import org.kohsuke.stapler.verb.POST;
 
-import jenkins.model.Jenkins;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import java.io.File;
+
+import hudson.model.AbstractProject;
+import hudson.model.Descriptor;
+import hudson.util.ListBoxModel;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.SynchronousNonBlockingStepExecution;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+
+import hudson.model.AbstractProject;
+import hudson.model.Descriptor;
+import hudson.util.ListBoxModel;
+
+import java.util.Queue;
+
+import javax.servlet.ServletException;
+
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper;
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import hudson.model.Item;
+import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
+
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
+import hudson.security.ACL;
+import jenkins.model.Jenkins;
+import hudson.util.ListBoxModel;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
+import hudson.plugins.git.SubmoduleConfig;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
 
 
 public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
@@ -59,13 +110,48 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
     private String dockerCredentialsId;
     private String SUDO_password;
     private String repositoryUrl;
+    private String credentialsId;
     private boolean enableGitCheckout;
+
+    public ListBoxModel doFillGitCredentialsIdItems() {
+        ListBoxModel result = new ListBoxModel();
+
+        // Add all credentials to the dropdown
+        for (StandardUsernamePasswordCredentials cred : CredentialsProvider.lookupCredentials(
+                StandardUsernamePasswordCredentials.class, Jenkins.getInstanceOrNull(), ACL.SYSTEM, Collections.emptyList())) {
+            result.add(cred.getUsername(), cred.getId());
+        }
+
+        return result;
+    }
+
+    @RequirePOST
+    public ListBoxModel doFillCredentialsIdItems(@AncestorInPath AbstractProject project) {
+        StandardListBoxModel result = new StandardListBoxModel();
+        result.includeEmptyValue();
+        result.includeMatchingAs(
+                ACL.SYSTEM,
+                Jenkins.getInstanceOrNull(),
+                StandardCredentials.class,
+                Collections.emptyList(),
+                CredentialsMatchers.always()
+        );
+        return result;
+    }
+    
+
     @DataBoundConstructor
-    public HelloWorldBuilder(String name) {
-        this.name = name;
-        this.gitCredentialsId = gitCredentialsId;
-        this.dockerCredentialsId = dockerCredentialsId;
-        this.SUDO_password = SUDO_password;
+public HelloWorldBuilder(String name, String gitCredentialsId, String dockerCredentialsId, String SUDO_password, String credentialsId) {
+    this.name = name;
+    this.gitCredentialsId = gitCredentialsId;
+    this.dockerCredentialsId = dockerCredentialsId;
+    this.SUDO_password = SUDO_password;
+    this.credentialsId = credentialsId;
+}
+
+
+    public String getCredentialsId() {
+        return this.credentialsId;
     }
 
     @DataBoundSetter
@@ -91,7 +177,6 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         this.useFrench = useFrench;
     }
 
-    
     private void checkoutRepository(FilePath workspace, TaskListener listener, Run<?, ?> run, Launcher launcher, String repositoryUrl, StandardUsernamePasswordCredentials gitCredentials)
     throws InterruptedException, IOException {
     try {
@@ -131,60 +216,69 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
     
         
               Jenkins jenkins = Jenkins.get();
-    if (jenkins == null) {
-        listener.getLogger().println("Jenkins instance is null.");
-        return;
-    }
+            if (jenkins == null) {
+                listener.getLogger().println("Jenkins instance is null.");
+                return;
+            }
 
-    if (enableGitCheckout && repositoryUrl != null && !repositoryUrl.isEmpty()) {
-        checkoutRepository(workspace, listener, run, launcher, repositoryUrl, gitCredentials);
+            if (enableGitCheckout && repositoryUrl != null && !repositoryUrl.isEmpty()) {
+                checkoutRepository(workspace, listener, run, launcher, repositoryUrl, gitCredentials);
 
-    }
+            }
 
   
-  // Read the Jenkinsfile from the plugin resources
-InputStream jenkinsfileStream = getClass().getClassLoader().getResourceAsStream("Jenkinsfile");
-if (jenkinsfileStream == null) {
-    listener.getLogger().println("Jenkinsfile resource not found in plugin.");
-    return;
-}
-
-        // Create a FilePath object for the destination
-        FilePath jenkinsfilePath = new FilePath(workspace, "Jenkinsfile");
-
-        // Copy the Jenkinsfile to the workspace
-        jenkinsfilePath.copyFrom(jenkinsfileStream);
-
-        listener.getLogger().println("Jenkinsfile copied to workspace.");
-
-    if (!jenkinsfilePath.exists()) {
-        listener.getLogger().println("Jenkinsfile does not exist in the workspace.");
+    // Read the Jenkinsfile from the plugin resources
+    InputStream jenkinsfileStream = getClass().getClassLoader().getResourceAsStream("Jenkinsfile");
+    if (jenkinsfileStream == null) {
+        listener.getLogger().println("Jenkinsfile resource not found in plugin.");
         return;
     }
 
-    String jenkinsfileContent = jenkinsfilePath.readToString();
+            // Create a FilePath object for the destination
+            FilePath jenkinsfilePath = new FilePath(workspace, "Jenkinsfile");
 
-        WorkflowJob job = (WorkflowJob) jenkins.getItemByFullName("MyUniquePipelineJob");
+            // Copy the Jenkinsfile to the workspace
+            jenkinsfilePath.copyFrom(jenkinsfileStream);
+
+            listener.getLogger().println("Jenkinsfile copied to workspace.");
+
+        if (!jenkinsfilePath.exists()) {
+            listener.getLogger().println("Jenkinsfile does not exist in the workspace.");
+            return;
+        }
+
+        String jenkinsfileContent = jenkinsfilePath.readToString();
+
+            // Delete existing pipeline job if it exists
+        WorkflowJob existingJob = Jenkins.getInstance().getItem("MyPipelineJob", Jenkins.getInstance(), WorkflowJob.class);
+        if (existingJob != null) {
+            existingJob.delete();
+        }
+
+        // Create new pipeline job
+        WorkflowJob job = Jenkins.getInstance().createProject(WorkflowJob.class, "MyPipelineJob");
+
+        // Check if the job creation was successful
         if (job == null) {
-            // Job doesn't exist, create it
-            job = new WorkflowJob(jenkins, "MyUniquePipelineJob");
-            jenkins.add(job, "MyUniquePipelineJob");
+            throw new IOException("Failed to create MyPipelineJob");
         }
-        // Set or update the job definition
-        job.setDefinition(new CpsFlowDefinition(jenkinsfileContent, true));
 
-        listener.getLogger().println("Jenkinsfile executing as a Pipeline job.");
-        
-        // Schedule the job
-        job.scheduleBuild2(0);
-        
-        // Wait for the job to start
-        WorkflowRun workflowRun = job.scheduleBuild2(0).waitForStart();
 
-        // Wait for the job to complete
-        while (workflowRun.getResult() == null) {
-            Thread.sleep(1000); // Sleep for 1 second before checking again
-        }
+            // Set or update the job definition
+            job.setDefinition(new CpsFlowDefinition(jenkinsfileContent, true));
+
+            listener.getLogger().println("Jenkinsfile executing as a Pipeline job.");
+            
+            // Schedule the job
+            job.scheduleBuild2(0);
+            
+            // Wait for the job to start
+            WorkflowRun workflowRun = job.scheduleBuild2(0).waitForStart();
+
+            // Wait for the job to complete
+            while (workflowRun.getResult() == null) {
+                Thread.sleep(1000); // Sleep for 1 second before checking again
+            }
 
             // Get the console output of the completed job
             Run<?, ?> completedRun = ((Job<?, ?>) jenkins.getItemByFullName("MyUniquePipelineJob")).getLastBuild();
@@ -195,6 +289,22 @@ if (jenkinsfileStream == null) {
             } else {
                 listener.getLogger().println("Failed to retrieve the console output of MyUniquePipelineJob.");
             }
+
+              // Execute a command with sudo using the sudo password provided
+        if (SUDO_password != null && !SUDO_password.isEmpty()) {
+            String commandWithSudo = "echo '" + SUDO_password + "' | sudo -S <your_command_here>";
+            // Replace <your_command_here> with the command you want to execute with sudo
+            // For example: "sudo -u someuser ls -l" or "sudo docker ps"
+            int exitCode = launcher.launch().cmdAsSingleString(commandWithSudo).pwd(workspace).stdout(listener).join();
+            if (exitCode != 0) {
+                listener.getLogger().println("Failed to execute command with sudo.");
+                // Handle failure gracefully or throw an exception if needed
+            }
+        } else {
+            listener.getLogger().println("SUDO password not provided. Skipping sudo command execution.");
+            // Handle the case where sudo password is not provided
+        }
+
 
         } catch (Exception e) {
             listener.getLogger().println("Error executing Jenkinsfile: " + e.getMessage());
